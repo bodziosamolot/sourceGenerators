8885c87214dcfa43a2bc6b69f434a6c75a8dd58e
Jarosław Ogiegło <jogieglo@future-processing.com>
2022-02-14 16:54:22 +0100

    started working

---
+++ SimpleExample/Test.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleExample;
+
+public static partial class Test
+{
+    public static void Execute()
+    {
+        PartialFunctionNotOnMain("Me");
+    }
+    
+    static partial void PartialFunctionNotOnMain(string name);
+}
